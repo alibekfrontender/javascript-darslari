64807dea15f492ff3e0e854e6c94101c0e5f4eeb
alibekfrontender <alibekfrontender@gmail.com>
2024-09-05 06:19:05 -0700

    javascript kodlarim

---
+++ sentaber/3-dars.js funksiya/main.js
@@ -0,0 +1,89 @@
+// function bek(a, b){
+//     console.log(a + b);
+//     return a + b;
+// }
+
+// function ali(a, b) {
+//     return a + b
+// }
+
+// let k = bek(4, 5)
+// let i = ali(4, 5)
+
+// console.log("k:", k);
+// console.log("i", i);
+
+
+
+
+
+// function five(number) {
+//     if(number % 5 === 0){
+//         return true
+//     }else {
+//         return false
+//     }
+// }
+
+
+// let x = five(10)
+// let y = five(17)
+
+// console.log(x);
+// console.log(y);
+
+// console.log(five(14));
+// console.log(five(prompt("son kiriting:")));
+// console.log(prompt(son));
+
+
+// function son(son) {
+//     if (son % 0 == 0) {
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+
+// let x = son(10)
+// let y = son(17)
+
+
+// console.log(x);
+// console.log(y);
+
+
+
+function day(son) {
+    if(son == 1){
+        console.log("dushanba");
+         
+    }else if (son == 2) {
+        console.log("seshanba");
+        
+    }else if (son == 3){
+        console.log("chorshanba");
+        
+    }else if(son == 4){
+        console.log("payshanba");
+        
+    }else if(son == 5){
+       console.log("juma");
+       
+    }else if(son == 6){
+        console.log("shanba");
+        
+    }else if(son == 7){
+        console.log("yakshanba");
+        
+    }else{
+        console.log("no'malun kun");
+    }
+}
+
+day(prompt("son kiriting"))
+
+
+
+
+
